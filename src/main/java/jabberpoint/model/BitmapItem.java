ce54660d73bd80b5583da94bf782652289dc08e1
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2026-04-05 15:51:24 +0200

    Implement Strategy pattern - rendering logic separated from model classes

--- src/main/java/jabberpoint/model/BitmapItem.java
+++ src/main/java/jabberpoint/model/BitmapItem.java
@@ -1,70 +1,37 @@
 package jabberpoint.model;
 
-import java.awt.Rectangle;
-import java.awt.Graphics;
+import jabberpoint.strategy.BitmapDrawStrategy;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
 import java.io.File;
-
+import java.io.IOException;
 import javax.imageio.ImageIO;
 
-import java.io.IOException;
-
-
-/** <p>De klasse voor een Bitmap item</p>
- * <p>Bitmap items have the responsibility to draw themselves.</p>
- * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
- * @version 1.6 2014/05/16 Sylvia Stuurman
-*/
-
+// SRP: BitmapItem only holds image data, rendering delegated to BitmapDrawStrategy
 public class BitmapItem extends SlideItem {
-  private BufferedImage bufferedImage;
-  private String imageName;
-  
-  protected static final String FILE = "File ";
-  protected static final String NOTFOUND = " not found";
+	private BufferedImage bufferedImage;
+	private String imageName;
 
-// level is equal to item-level; name is the name of the file with the Image
+	protected static final String FILE = "File ";
+	protected static final String NOTFOUND = " not found";
+
 	public BitmapItem(int level, String name) {
-		super(level);
+		super(level, null);
 		imageName = name;
 		try {
 			bufferedImage = ImageIO.read(new File(imageName));
+		} catch (IOException e) {
+			System.err.println(FILE + imageName + NOTFOUND);
 		}
-		catch (IOException e) {
-			System.err.println(FILE + imageName + NOTFOUND) ;
-		}
+		// Set strategy after image is loaded
+		this.drawStrategy = new BitmapDrawStrategy(bufferedImage);
 	}
 
-// An empty bitmap-item
 	public BitmapItem() {
 		this(0, null);
 	}
 
-// give the filename of the image
 	public String getName() {
 		return imageName;
-	}
-
-// give the  bounding box of the image
-	public Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style myStyle) {
-		return new Rectangle((int) (myStyle.indent * scale), 0,
-				(int) (bufferedImage.getWidth(observer) * scale),
-				((int) (myStyle.leading * scale)) + 
-				(int) (bufferedImage.getHeight(observer) * scale));
-	}
-
-// draw the image
-	public void draw(int x, int y, float scale, Graphics g, Style myStyle, ImageObserver observer) {
-		int width = x + (int) (myStyle.indent * scale);
-		int height = y + (int) (myStyle.leading * scale);
-		g.drawImage(bufferedImage, width, height,(int) (bufferedImage.getWidth(observer)*scale),
-                (int) (bufferedImage.getHeight(observer)*scale), observer);
 	}
 
 	public String toString() {
